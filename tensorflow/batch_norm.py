cb8da9a8fc1cc1f78315babdb87b0fbf6604dcca
mtbthebest <mtbthebest11@gmail.com>
2017-06-23 16:53:56 +0900

    s

--- tensorflow/batch_norm.py
+++ tensorflow/batch_norm.py
@@ -5,6 +5,8 @@ os.environ['TF_CPP_MIN_LOG_LEVLEL'] = '2'
 import tensorflow as tf
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.layers.batch_norm() import batch_norm
+
 
 n_inputs = 28*28
 n_hidden1 = 300
@@ -13,36 +15,18 @@ n_outputs = 10
 learning_rate = 0.01
 
 X= tf.placeholder(dtype = tf.float32, shape = (None, n_inputs), name = 'X')
-y= tf.placeholder(dtype = tf.int64, shape = (None), name = 'y')
+training = tf.placeholder_with_default(False, shape =(), name="training")
 
-def neuron_layer(X, n_neurons, name, activation=None):
-    with tf.name_scope(name):
-        n_inputs = int(X.get_shape()[1])
-        stddev = 2/np.sqrt(n_inputs)
-        init     = tf.truncated_normal((n_inputs, n_neurons) , stddev = stddev)
-        W = tf.Variable(init , name="weights")
-        b = tf.Variable(tf.zeros([n_neurons]), name = "biases")
-        z = tf.matmul(X,W) + b
-        if activation =="relu":
-            return tf.nn.relu(z)
-        else:
-            return z
-with tf.name_scope("dnn"):
-    hidden1= neuron_layer(X  ,n_hidden1,"hidden1", activation = "relu")
-    hidden2= neuron_layer(X  ,n_hidden2,"hidden2", activation = "relu")
-    logits = neuron_layer(hidden2, n_outputs,"outputs")
+hidden1= tf.layers.dense(X,n_hidden1,name = "hidden1")
+bn1 = tf.layers.batch_normalization(hidden1, trainable=training, momentum=0.9)
+bn1_act = tf.nn.elu(bn1)
 
-with tf.name_scope("loss"):
-    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = logits)
-    loss = tf.reduce_mean(xentropy, name = "loss")
+hidden2= tf.layers.dense(bn1_act,n_hidden2,name = "hidden2")
+bn2 = tf.layers.batch_normalization(hidden2, trainable=training, momentum=0.9)
+bn2_act = tf.nn.elu(bn2)
 
-with tf.name_scope("train"):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate)
-    training_op = optimizer.minimize(loss)
-
-with tf.name_scope("eval"):
-    correct = tf.nn.in_top_k(logits,y,1)
-    accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+logits_before_bn = tf.layers.dense(bn1_act,n_outputs,name = "outputs")
+logits = tf.layers.batch_normalization(logits_before_bn, training=training, momentum=0.9)
 
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
