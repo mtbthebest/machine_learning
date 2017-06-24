cd4b4704c24c134b5bf3139ac2ae038b5f9eb6fe
mtbthebest <mtbthebest11@gmail.com>
2017-06-24 20:22:24 +0900

    rnn train

--- tensorflow/seq_length.py
+++ tensorflow/seq_length.py
@@ -5,23 +5,31 @@ os.environ['TF_CPP_MIN_LOG_LEVLEL'] = '2'
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.rnn import BasicRNNCell, static_rnn
+
 n_steps = 2
 n_inputs = 3
 n_neurons = 5
+
 X = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
-X_seqs = tf.unstack(tf.transpose(X, perm=[1, 0, 2]))
+
 basic_cell = tf.contrib.rnn.BasicRNNCell(num_units=n_neurons)
-output_seqs, states = tf.contrib.rnn.static_rnn(basic_cell, X_seqs, dtype=tf.float32)
-outputs = tf.transpose(tf.stack(output_seqs), perm=[1, 0, 2])
+seq_length = tf.placeholder(tf.int32, [None])
+outputs, states = tf.nn.dynamic_rnn(basic_cell, X, dtype=tf.float32,
+                                    sequence_length=seq_length)
+
 init = tf.global_variables_initializer()
-X_batch = np.array([[[0, 1, 2], [9, 8, 7]], 
+
+X_batch = np.array([
+        [[0, 1, 2], [9, 8, 7]], 
         [[3, 4, 5], [0, 0, 0]], 
-        [[6, 7, 8], [6, 5, 4]],
+        [[6, 7, 8], [6, 5, 4]], 
         [[9, 0, 1], [3, 2, 1]], 
     ])
+seq_length_batch = np.array([2, 1, 2, 2])
 
 with tf.Session() as sess:
     init.run()
-    outputs_val = outputs.eval(feed_dict={X: X_batch})
+    outputs_val, states_val = sess.run(
+        [outputs, states], feed_dict={X: X_batch, seq_length: seq_length_batch})
 
-print outputs_val
+print(outputs_val)
