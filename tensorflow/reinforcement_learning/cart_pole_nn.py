9301f2e242ae871db9b096ad77d225cfe0a06f1f
mtbthebest <mtbthebest11@gmail.com>
2017-06-25 20:23:34 +0900

    rl

--- tensorflow/reinforcement_learning/cart_pole_nn.py
+++ tensorflow/reinforcement_learning/cart_pole_nn.py
@@ -10,61 +10,92 @@ import matplotlib.pyplot as plt
 
 tf.reset_default_graph()
 
+import tensorflow as tf
+
+
 n_inputs = 4
 n_hidden = 4
 n_outputs = 1
 
 learning_rate = 0.01
 
-initializer = variance_scaling_initializer()
-X = tf.placeholder(tf.float32, shape=[None,n_inputs])
-y = tf.placeholder(tf.float32, shape=[None,n_outputs])
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+X = tf.placeholder(tf.float32, shape=[None, n_inputs])
 
 hidden = tf.layers.dense(X, n_hidden, activation=tf.nn.elu, kernel_initializer=initializer)
 logits = tf.layers.dense(hidden, n_outputs)
-outputs = tf.nn.sigmoid(logits)
-p_left_and_right = tf.concat(axis=1, values =[outputs, 1-outputs])
-action = tf.multinomial(tf.log(p_left_and_right), num_samples = 1)
+outputs = tf.nn.sigmoid(logits)  
+p_left_and_right = tf.concat(axis=1, values=[outputs, 1 - outputs])
+action = tf.multinomial(tf.log(p_left_and_right), num_samples=1)
+
+y = 1. - tf.to_float(action)
 cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=logits)
-optimizer= tf.train.AdamOptimizer(learning_rate=0.001)
-training_op = optimizer.minimize(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate)
+grads_and_vars = optimizer.compute_gradients(cross_entropy)
+gradients = [grad for grad, variable in grads_and_vars]
+gradient_placeholders = []
+grads_and_vars_feed = []
+for grad, variable in grads_and_vars:
+    gradient_placeholder = tf.placeholder(tf.float32, shape=grad.get_shape())
+    gradient_placeholders.append(gradient_placeholder)
+    grads_and_vars_feed.append((gradient_placeholder, variable))
+training_op = optimizer.apply_gradients(grads_and_vars_feed)
 
 init = tf.global_variables_initializer()
 saver = tf.train.Saver()
 
-def render_policy_net(model_path, action, X, n_max_steps = 1000):
-    frames = []
-    env = gym.make("CartPole-v0")
-    obs = env.reset()
-    with tf.Session() as sess:
-        saver.restore(sess, model_path)
-        for step in range(n_max_steps):
-            img = render_cart_pole(env, obs)
-            frames.append(img)
-            action_val = action.eval(feed_dict={X: obs.reshape(1, n_inputs)})
-            obs, reward, done, info = env.step(action_val[0][0])
-            if done:
-                break
-    env.close()
-    return framess
 
-if __name__ == '__main__':
-    n_environments = 10
-    n_iterations = 1000
-    envs = [gym.make("CartPole-v0") for _ in range(n_environments)]
-    observations = [env.reset() for env in envs]
-    
-    with tf.Session() as sess:
-        init.run()
-        for iteration in range(n_iterations):
-            target_probas = np.array([([1.] if obs[2] < 0 else [0.]) for obs in observations]) 
-            action_val, _ = sess.run([action, training_op], feed_dict={X: np.array(observations), y: target_probas})
-            for env_index, env in enumerate(envs):
-                obs, reward, done, info = env.step(action_val[env_index][0])
-                observations[env_index] = obs if not done else env.reset()
-        saver.save(sess, "./my_policy_net_basic.ckpt")
+def discount_rewards(rewards, discount_rate):
+    discounted_rewards = np.zeros(len(rewards))
+    cumulative_rewards = 0
+    for step in reversed(range(len(rewards))):
+        cumulative_rewards = rewards[step] + cumulative_rewards * discount_rate
+        discounted_rewards[step] = cumulative_rewards
+    return discounted_rewards
 
-   
-        frames = render_policy_net("./my_policy_net_basic.ckpt", action, X)
-        video = plot_animation(frames)
-        plt.show()
+def discount_and_normalize_rewards(all_rewards, discount_rate):
+    all_discounted_rewards = [discount_rewards(rewards, discount_rate) for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
+
+env = gym.make("CartPole-v0")
+
+n_games_per_update = 10
+n_max_steps = 1000
+n_iterations = 250
+save_iterations = 10
+discount_rate = 0.95
+
+with tf.Session() as sess:
+    init.run()
+    for iteration in range(n_iterations):
+        print("\rIteration: {}".format(iteration))
+        all_rewards = []
+        all_gradients = []
+        for game in range(n_games_per_update):
+            current_rewards = []
+            current_gradients = []
+            obs = env.reset()
+            for step in range(n_max_steps):
+                action_val, gradients_val = sess.run([action, gradients], feed_dict={X: obs.reshape(1, n_inputs)})
+                obs, reward, done, info = env.step(action_val[0][0])
+                current_rewards.append(reward)
+                current_gradients.append(gradients_val)
+                if done:
+                    break
+            all_rewards.append(current_rewards)
+            all_gradients.append(current_gradients)
+
+        all_rewards = discount_and_normalize_rewards(all_rewards, discount_rate=discount_rate)
+        feed_dict = {}
+        for var_index, gradient_placeholder in enumerate(gradient_placeholders):
+            mean_gradients = np.mean([reward * all_gradients[game_index][step][var_index]
+                                      for game_index, rewards in enumerate(all_rewards)
+                                          for step, reward in enumerate(rewards)], axis=0)
+            feed_dict[gradient_placeholder] = mean_gradients
+        sess.run(training_op, feed_dict=feed_dict)
+        if iteration % save_iterations == 0:
+            saver.save(sess, "./my_policy_net_pg.ckpt")
