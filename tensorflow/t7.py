7986627fc05620c239a13cbc1649af1af95f959d
Unknown <mtbthebest11@gmail.com>
2017-10-23 12:58:20 +0900

    dataset

--- tensorflow/t7.py
+++ tensorflow/t7.py
@@ -8,8 +8,8 @@ import numpy as np
 
 #Dataset 1
 
-path_dir = '/home/mtb/Downloads/'
-filename= path_dir + 'port.csv'
+path_dir = '/home/mtb/Documents/data/train/'
+filename= path_dir + 'train_2.csv'
 
 a = np.array([[1,2,3,4],[5,6,7,8]])
 
@@ -19,30 +19,45 @@ def dataset1():
     iterator = dataset1.make_initializable_iterator()
     next_elem = iterator.get_next()
 
-with tf.Session() as sess:
-    sess.run(iterator.initializer)
-    while True:
-        try:
-            b = sess.run(next_elem)
-            print b
-        except tf.errors.OutOfRangeError:
-            break
+    with tf.Session() as sess:
+        sess.run(iterator.initializer)
+        while True:
+            try:
+                b = sess.run(next_elem)
+                print b
+            except tf.errors.OutOfRangeError:
+                break
     
     
 #Dataset 2
 
-a = np.array([[1,2,3,4],[5,6,7,8]])
 
-with tf.Session() as sess:
-    sess.run(iterator.initializer)
-    while True:
-        try:
-            b = sess.run(next_elem)
-            print b
-        except tf.errors.OutOfRangeError:
-            break
-    
+def dataset2():
+    with tf.Session() as sess:
+        sess.run(iterator.initializer)
+        while True:
+            try:
+                b = sess.run(next_elem)
+                print b
+            except tf.errors.OutOfRangeError:
+                break
 
+
+filename_queue = tf.train.string_input_producer([filename])
+line_reader = tf.TextLineReader(skip_header_lines=1)
+key, value = line_reader.read(filename_queue)
+# val = decode_csv(value)
+# record_defaults = [[0.0], [0.0]]
+
+# inputs, outputs = tf.decode_csv(records=value, record_defaults= record_defaults)
 if __name__ == '__main__':
-    main()
-    
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+  
+        for i in range(2):
+            features, labels = sess.run([key,value])
+            print labels[2:-3].split(',')
+        coord.request_stop()
+        coord.join(threads)
